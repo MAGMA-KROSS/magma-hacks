63258b9c4e79e923175b633d4e55b35bde66e9ce
MAGMA_KROSS <abik4002@gmaim>
2025-06-01 15:46:02 +0530

    side bar fixes

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,6 +3,15 @@ import { useState } from 'react';
 import { ModeToggle } from './ModeToggle';
 import Link from 'next/link.js';
 import { usePathname } from 'next/navigation';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Footer from './Footer';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,43 +33,69 @@ export default function Navbar() {
         <div className="flex items-center justify-center gap-1">
           <div className="block md:hidden">
             <ModeToggle />
+
+            <Sheet>
+              <SheetTrigger>
+
+                <svg className="w-5 h-5 mx-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>MagmaHacks</SheetTitle>
+                  <SheetDescription asChild>
+                    <div className="transition-all duration-100 w-full md:block md:w-auto" id="navbar-default">
+                      <ul className="font-medium md:hidden flex flex-col  md:p-0 mt-4 borde items-center rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
+                        <li className={`w-full md:w-min text-center `}>
+                          <Link href="/" className={`${isActive('/') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3    rounded-sm md:bg-transparent  md:p-0 dark:text-white `} aria-current="page">Home</Link>
+                        </li>
+                        <li className="w-full md:w-min text-center">
+                          <Link href="About" className={`${isActive('/About') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>About</Link>
+                        </li>
+
+                        <li className="w-full md:w-min text-center">
+                          <Link href="Pricing" className={`${isActive('/Pricing') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>Pricing</Link>
+                        </li>
+                        <li className="w-full md:w-min text-center">
+                          <Link href="Contact" className={`${isActive('/Contact') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>Contact</Link>
+                        </li>
+                        <li className="w-full md:w-min text-center">
+                          <Link href="Blogs" className={`${isActive('/Blogs') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>Blogs</Link>
+                        </li>
+                      </ul>
+                    
+                    </div>
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
           </div>
-          <button
-            type="button"
-            onClick={() => setIsOpen(!isOpen)}
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-expanded={isOpen}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-            </svg>
-          </button>
+          <div className="transition-all hidden duration-100 w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium  flex flex-col p-4 md:p-0 mt-4 border border-gray-100 items-center rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
+              <li className="w-full md:w-min text-center ">
+                <Link href="/" className={`${isActive('/') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3  rounded-sm md:bg-transparent  md:p-0 dark:text-white `} aria-current="page">Home</Link>
+              </li>
+              <li className="w-full md:w-min text-center">
+                <Link href="About" className={`${isActive('/About') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>About</Link>
+              </li>
+
+              <li className="w-full md:w-min text-center">
+                <Link href="Pricing" className={`${isActive('/Pricing') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>Pricing</Link>
+              </li>
+              <li className="w-full md:w-min text-center">
+                <Link href="Contact" className={`${isActive('/Contact') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>Contact</Link>
+              </li>
+              <li className="w-full md:w-min text-center">
+                <Link href="Blogs" className={`${isActive('/Blogs') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>Blogs</Link>
+              </li>
+              <li className="md:block hidden cursor-pointer">
+                <ModeToggle />
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div className={`${isOpen ? 'block' : 'hidden'} transition-all duration-100 w-full md:block md:w-auto`} id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 items-center rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
-            <li className={`w-full md:w-min text-center `}>
-              <Link href="/" className={`${isActive('/') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3    rounded-sm md:bg-transparent  md:p-0 dark:text-white `} aria-current="page">Home</Link>
-            </li>
-            <li className="w-full md:w-min text-center">
-              <Link href="About" className={`${isActive('/About') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>About</Link>
-            </li>
-
-            <li className="w-full md:w-min text-center">
-              <Link href="Pricing" className={`${isActive('/Pricing') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>Pricing</Link>
-            </li>
-            <li className="w-full md:w-min text-center">
-              <Link href="Contact" className={`${isActive('/Contact') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>Contact</Link>
-            </li>
-            <li className="w-full md:w-min text-center">
-              <Link href="Blogs" className={`${isActive('/Blogs') ? "md:text-[#ea580c] text-white md:dark:text-[#ea580c] bg-[#ea580c] dark:md:bg-transparent" : ""} block dark:hover:text-[#ea580c] py-2 px-3   rounded-sm md:bg-transparent  md:p-0 dark:text-white `}>Blogs</Link>
-            </li>
-            <li className="md:block hidden cursor-pointer">
-              <ModeToggle />
-            </li>
-          </ul>
-        </div>
       </div>
     </nav>
   );
